d1022956e304a2d8364e9754451345b45359f661
virasj <>
2003-02-25 09:50:10 +0000

    example

---
+++ v3/wordcut/example/wcdict_ex.c
@@ -0,0 +1,13 @@
+#include<wordcut/wcdict.h>
+int 
+main()
+{
+  WcDict *dict;
+  WcDictIter iter;
+  dict=wc_dict_new();
+  wc_dict_load(dict,"../data/dict.etd");
+  wc_dict_root(dict,&iter);
+  wc_dict_delete(dict);
+  return 0;
+}
+
