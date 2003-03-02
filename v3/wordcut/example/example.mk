ff207e2a5fd25f540eb8a251b05a2086dbfdf98a
virasj <>
2003-03-02 13:42:56 +0000

    example fixing

--- v3/wordcut/example/example.mk
+++ v3/wordcut/example/example.mk
@@ -1,7 +1,11 @@
-CC=gcc
-LDFLAGS=`pkg-config wordcut --libs`
+CC=gcc LDFLAGS=`pkg-config wordcut --libs`
 CFLAGS=`pkg-config wordcut --cflags` -Wall -g
-all : test1
+all : test1 wcdict_ex wcdictiter_ex wcdictiterpos_ex wcdictmap_ex wcwordcut_ex
 test1 : test1.c
+wcdict_ex : wcdict_ex.c
+wcdictiter_ex : wcdictiter_ex.c
+wcdictiterpos_ex : wcdictiterpos_ex.c
+wcdictmap_ex : wcdictmap_ex.c
+wcwordcut_ex : wcwordcut_ex.c
 clean :
-	rm -f test1 test1.o core
+	rm -f test1 *.o core wcdict_ex wcdictiter_ex wcdictiterpos_ex wcdictmap_ex wcwordcut_ex
